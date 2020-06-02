aebe4a4960860fa6da18c40010514d93a6d24a1c
Nathalia <nathaliamelare@gmail.com>
2020-06-01 21:17:50 -0300

    Botton remove from storyboard and added by code

--- Call_Test_/Call_Test/ViewController.swift
+++ Call_Test_/Call_Test/ViewController.swift
@@ -9,20 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var btnCall: UIButton!
     
     var phone: String = ""
-   
-    @IBAction func btnCallTest(_ sender: Any) {
-        if let url = URL(string: "tel://\(phone)"),
-        UIApplication.shared.canOpenURL(url) {
-        UIApplication.shared.open(url, options: [:], completionHandler: nil)
-    }
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUpButton()
+    }
+    
+    func setUpButton() {
+        let button = UIButton(type: .system)
+        button.frame = CGRect(x: self.view.frame.width/3, y: self.view.frame.height/2, width: 150, height: 75)
+        button.backgroundColor = .systemGreen
+        button.setTitle("Call", for: UIControl.State.normal)
+        button.setTitleColor(.white, for: UIControl.State.normal)
+        button.titleLabel?.font = .systemFont(ofSize: 40)
+        button.addTarget(self, action: #selector(call(sender:)), for: UIControl.Event.touchDown)
+        self.view.addSubview(button)
+    }
+    
+    @objc func call(sender: UIButton!) {
+        if let url = URL(string: "tel://\(phone)"),
+            UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
     }
     
 }
